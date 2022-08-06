055b39290046743c8a893ab8b256f852cd356801
Brian Li <brian14708@gmail.com>
2022-08-06 23:44:16 +0800

    Add clip text encoder

---
+++ core/src/ldm/clip.rs
@@ -0,0 +1,63 @@
+use std::io::Read;
+use std::path::Path;
+use std::str::FromStr;
+
+use tokenizers::Tokenizer;
+
+use crate::ort::Session;
+use crate::result::Error;
+use crate::result::Result;
+use crate::text_encoder::TextEncoder;
+
+pub struct ClipEncoder {
+    tokenizer: Tokenizer,
+    session: Session,
+}
+
+impl TextEncoder for ClipEncoder {
+    fn encode(&mut self, inp: &[String]) -> Result<ndarray::ArrayD<f32>> {
+        let enc = self
+            .tokenizer
+            .encode_batch(inp.iter().map(|s| s.as_str()).collect(), true)
+            .map_err(|e| Error::Tokenizer(e))?;
+        if enc.is_empty() {
+            return Ok(ndarray::ArrayD::zeros([0].as_slice()));
+        }
+        let enc = ndarray::Array2::from_shape_vec(
+            (enc.len(), enc[0].len()),
+            enc.iter()
+                .flat_map(|e| e.get_ids())
+                .map(|&v| v as i64)
+                .collect(),
+        )
+        .unwrap();
+
+        let mut run = self.session.prepare();
+        run.set_input("input", &enc)?;
+        let out = run.exec()?;
+        let out = out.get_output_idx::<f32, ndarray::Ix2>(0)?;
+        Ok(out.into_dyn().to_owned())
+    }
+}
+
+impl ClipEncoder {
+    pub fn new(path: &Path) -> Result<Self> {
+        let mut ar = tsar::Archive::new(std::fs::File::open(path)?)?;
+        let mut buf = String::new();
+        ar.file_by_name("tokenizer.json")?
+            .read_to_string(&mut buf)?;
+        let mut tokenizer = Tokenizer::from_str(&buf).map_err(|e| Error::Tokenizer(e))?;
+        tokenizer
+            .with_padding(Some(tokenizers::PaddingParams {
+                strategy: tokenizers::PaddingStrategy::Fixed(77),
+                ..Default::default()
+            }))
+            .with_truncation(Some(tokenizers::TruncationParams {
+                max_length: 77,
+                ..Default::default()
+            }));
+
+        let session = Session::load(&mut ar, "textual.onnx")?;
+        Ok(Self { tokenizer, session })
+    }
+}
--- core/src/text_encoder.rs
+++ core/src/text_encoder.rs
@@ -1,6 +1,6 @@
 use std::path::Path;
 
-use crate::ldm::bert;
+use crate::ldm::{bert, clip};
 use crate::result::{Error, Result};
 
 pub trait TextEncoder {
@@ -10,6 +10,7 @@ pub trait TextEncoder {
 pub fn load(kind: impl AsRef<str>, path: impl AsRef<Path>) -> Result<Box<dyn TextEncoder>> {
     match kind.as_ref() {
         "ldm/bert" => Ok(Box::new(bert::BertEncoder::new(path.as_ref())?)),
+        "ldm/clip" => Ok(Box::new(clip::ClipEncoder::new(path.as_ref())?)),
         k => Err(Error::UnsupportedModel(
             "text encoder".to_string(),
             k.to_owned(),
